7c399bab26f32d8ba392578f2b43e2fc717297ac
Asef90 <asef.dag90@gmail.com>
2020-03-01 05:15:55 +0300

    Added scopes and validations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,13 @@
 class Question < ApplicationRecord
   has_many :answers
   belongs_to :test
+
+  validates :body, presence: true
+  validates :questions, on: :update
+
+  private
+
+  def validate_answers_number
+    errors.add(:question, 'may contain 1-4 answers') unless answers.size.between?(1, 4)
+  end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -5,6 +5,14 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User'
 
+  validates :title, presence: true
+  validates :level, numericality: { only_integer: true }
+
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+  scope :by_level, -> (level) { where(level: level) }
+
   def self.by_category(category)
     joins(:category).where(categories: { title: category })
                     .order(title: :desc).pluck(:title)
