6c25763d08df78b5be36fd83edf7c6626880aa89
Asef90 <asef.dag90@gmail.com>
2020-03-05 21:39:07 +0300

    Create CRUD questions

---
+++ app/controllers/tests_controller.rb
@@ -0,0 +1,18 @@
+class TestsController < ApplicationController
+  before_action :find_test, only: :show
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
+
+  def show
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
+
+  def rescue_with_test_not_found
+    render plain: "Test not found"
+  end
+end
