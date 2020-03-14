f2e7f59782aa6c5295def72dd9719ec01424597a
Asef90 <asef.dag90@gmail.com>
2020-03-15 00:34:29 +0300

    Add sessions/login 2

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,7 +3,7 @@ class Test < ApplicationRecord
   has_many :test_passages
   has_many :users, through: :test_passages
   belongs_to :category
-  belongs_to :author, class_name: 'User', optional: true
+  belongs_to :author, class_name: 'User'
 
   validates :title, presence: true,
                     uniqueness: { scope: :level }
