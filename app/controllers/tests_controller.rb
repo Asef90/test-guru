13ae83d91b37e0e83d33208db3032835899f3253
Asef90 <asef.dag90@gmail.com>
2020-03-20 05:00:14 +0300

    Add devise/mail

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,7 +1,6 @@
 class TestsController < ApplicationController
 
   before_action :find_test, only: %i[show edit update destroy start]
-  #before_action :find_user, only: :start
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
@@ -13,14 +12,14 @@ class TestsController < ApplicationController
   end
 
   def new
-    @test = @current_user.created_tests.new
+    @test = current_user.created_tests.new
   end
 
   def edit
   end
 
   def create
-    @test = @current_user.created_tests.new(test_params)
+    @test = current_user.created_tests.new(test_params)
     if @test.save
       redirect_to @test
     else
@@ -42,8 +41,8 @@ class TestsController < ApplicationController
   end
 
   def start
-    @current_user.tests.push(@test)
-    redirect_to @current_user.test_passage(@test)
+    current_user.tests.push(@test)
+    redirect_to current_user.test_passage(@test)
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,17 @@
 class User < ApplicationRecord
-  EMAIL_FORMAT = /\A\S+@\w+\.\w{2,6}\z/i
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable,
+         :confirmable
 
   has_many :created_tests, foreign_key: :author_id, class_name: 'Test'
   has_many :test_passages
   has_many :tests, through: :test_passages
-
-  has_secure_password
-
-  validates :email, presence: true,
-                    uniqueness: true,
-                    format: EMAIL_FORMAT
 
   def test_passage(test)
     test_passages.where(test_id: test.id).last
