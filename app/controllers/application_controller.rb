2b1c85938c40f72576bf2921c7e119fd65b02761
Asef90 <asef.dag90@gmail.com>
2020-03-21 03:35:43 +0300

    Add first/last name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
+
+  def after_sign_in_path_for(resource)
+    resource.is_a?(Admin) ? admin_tests_path : super
+  end
 
 end
