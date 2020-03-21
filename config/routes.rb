d46ec44941401b7b6af9456b5aed44bc23e631e2
Asef90 <asef.dag90@gmail.com>
2020-03-21 04:22:47 +0300

    Lesson 11

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'tests#index'
 
-  devise_for :users, path_names: { sign_in: :login, sign_out: :logout }
+  devise_for :users, path_names: { sign_in: :login, sign_out: :logout },
+             controllers: { sessions: 'users/sessions' }
 
   resources :tests, only: :index do
     member do
