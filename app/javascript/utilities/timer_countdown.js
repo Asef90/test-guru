5453cc17a531d35ae31edf81d12b8b23feca9362
Asef90 <asef.dag90@gmail.com>
2020-04-20 11:51:21 +0300

    Lesson 18 pt.2 upd.2

--- app/javascript/utilities/timer_countdown.js
+++ app/javascript/utilities/timer_countdown.js
@@ -3,27 +3,31 @@ document.addEventListener('turbolinks:load', function() {
 
 
   if (timerField) {
-    var finishTime = timerField.textContent;
-    var myTimer = setInterval(countDown, 1000, finishTime, timerField);
+    var finishedTime = timerField.dataset.finishTime;
+    var now = new Date();
+    var remainTime = (new Date(finishedTime) - now) / 1000;
+
+    startTimer(remainTime, timerField);
   }
 })
 
-function countDown(finishTime, timerField) {
-  var now = new Date();
-  var remainTime = new Date(finishTime) - now;
-  //var remainSeconds = new Date(finishTime).getSeconds() - now.getSeconds();
-  var remainMinutes = Math.floor(remainTime / 1000 / 60);
-  var remainSeconds = Math.floor(remainTime / 1000 % 60);
+function startTimer(time, field) {
+  var redirectPath = window.location.href + '/result';
 
-  if (remainMinutes < 0 && remainSeconds < 0) {
-    timerField.style.color = "red";
-    clearInterval(myTimer);
-  }
+  var timer = time, minutes, seconds;
+  var timerOn = setInterval(function() {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
 
-  remainMinutes = remainMinutes < 10 ? "0" + remainMinutes : remainMinutes;
-  remainSeconds = remainSeconds < 10 ? "0" + remainSeconds : remainSeconds;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
 
+    field.innerHTML = "Timer: " + minutes + " : " + seconds;
 
-  timerField.innerHTML = "Timer: " + remainMinutes + " : " + remainSeconds;
-  timerField.classList.remove('hide');
+    if (--timer < 0) {
+      clearInterval(timerOn);
+      alert("Time is over");
+      window.location.href = redirectPath;
+    }
+  }, 1000)
 }
